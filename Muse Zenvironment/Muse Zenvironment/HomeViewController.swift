b1638eab8dbf793438d572f1a361556cbd84e518
eteters <evan@teters.net>
2019-02-06 13:47:00 -0600

    Implement goof animation as proof of concept

--- Muse Zenvironment/Muse Zenvironment/HomeViewController.swift
+++ Muse Zenvironment/Muse Zenvironment/HomeViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class HomeViewController: UIViewController, StreamDelegate {
 
+    @IBOutlet weak var getRelaxed: UILabel!
     let headbandReceiver = HeadbandReceiver()
     
     
     override func viewWillAppear(_ animated: Bool){
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
         headbandReceiver.delegate = self
         headbandReceiver.setupNetworkConnection()
     }
@@ -23,6 +25,13 @@ class HomeViewController: UIViewController, StreamDelegate {
 extension HomeViewController:HeadbandReceiverDelegate {
     func receivedMessage(message: HeadbandMessage) {
         print("message in VC with alpha at \(message.alphaRelaxation)")
-        
+        let red   = CGFloat(message.alphaRelaxation) //* 255.0
+        let green = CGFloat(message.betaConcentration) //* 255.0
+        let blue  = CGFloat(message.thetaRelaxation) //* 255.0
+        let alpha = CGFloat(1.0)
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.repeat,.autoreverse], animations: {
+            self.view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+            self.getRelaxed.textColor = UIColor(red: 1-red, green: 1-green, blue: 1-blue, alpha: alpha)
+        }, completion: nil)
     }
 }
